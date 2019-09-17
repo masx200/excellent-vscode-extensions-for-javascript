bbbdbc6f2bb02a351bcacd8717f3b5231ac75acc
masx200 <34191203+masx200@users.noreply.github.com>
2019-09-17 08:59:06 +0800

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,58 @@
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+import json from "rollup-plugin-json";
+import typescript from "rollup-plugin-typescript";
+const myterserplugin = terser({
+  sourcemap: true,
+  toplevel: true,
+  output: {
+    comments: !1,
+    ascii_only: !0
+  },
+  compress: {
+    toplevel: true,
+    unused: true,
+    drop_console: true,
+    drop_debugger: true,
+    pure_funcs: ["console.log"]
+  },
+  mangle: { properties: false }
+});
+export default [
+  {
+    input: "./src/index.ts",
+    output: [
+      {
+        file: "./dist/index.js",
+        format: "esm",
+        sourcemap: true
+      }
+    ],
+    plugins: [
+      json(),
+      resolve(),
+      commonjs(),
+      typescript(),
+      terser({
+        compress: false,
+        mangle: false,
+        output: {
+          comments: !1,
+          beautify: true
+        }
+      })
+    ]
+  },
+  {
+    input: "./dist/index.js",
+    output: [
+      {
+        file: "./dist/index.min.js",
+        format: "esm",
+        sourcemap: true
+      }
+    ],
+    plugins: [resolve(), commonjs(), myterserplugin]
+  }
+];
